5db1ef0854e09942ad9c6cb39e14d24671a0bf47
Augustus Odena <odenaaugustus@gmail.com>
2015-09-22 21:38:37 -0700

    Remove unnecessary print statements from ntm code.

--- ntm.py
+++ ntm.py
@@ -73,7 +73,6 @@ class NTM(object):
     self.w_ws_initInit = np.random.randn(self.N,1)*0.01
     self.w_rs_initInit = np.random.randn(self.N,1)*0.01
 
-    # TODO: make sure these are up to date
     self.weights = [   self.Wxh
                      , self.Wrh
                      , self.Who
@@ -166,10 +165,6 @@ class NTM(object):
       mems = {} # the state of the memory at every timestep
       # rs stores the value read out of memory
       rs[-1] = self.rsInit 
-      #print "first rs"
-      #print rs[-1].shape
-      # TODO: if we are not allowing neg values in otherwise
-      # then how much sense does it make to init mem to some negs
       mems[-1] = self.memsInit 
       w_ws_init = self.w_ws_initInit
       w_rs_init = self.w_rs_initInit
@@ -177,24 +172,11 @@ class NTM(object):
       w_rs[-1] = softmax(w_rs_init)
       loss = 0
 
-      if False:
-        print "w_ws[-1]"
-        print w_ws[-1]
-        print "w_rs[-1]"
-        print w_rs[-1]
-
       for t in xrange(len(inputs)):
-
-        if False:
-          print "t:", t
 
         xs[t] = np.reshape(np.array(inputs[t]),inputs[t].shape[::-1])
 
-        #TODO: could this just be a ReLu?
         hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Wrh, np.reshape(rs[t-1],(self.M,1))) + bh)
-        # TODO: seems like we could turn this into RNN controller
-        # simply by mapping o back around using affine into h[t+1]
-        # along with x[t+1] and r_t
 
         # o is analogous to o' in the LTT paper
         # TODO: in CGT, he doesn't activate what is his equivalent of the o layer
@@ -202,9 +184,6 @@ class NTM(object):
         os[t] = np.tanh(np.dot(Who, hs[t]) + bo)
 
         # parameters to the read head
-        # TODO: play with rectification of beta and gamma
-        # TODO: what are allowed values in memory?
-        # that will affect what values k should take
         k_rs[t] = np.tanh(np.dot(Wok_r,os[t]) + bk_r)
         beta_rs[t] = softplus(np.dot(Wobeta_r,os[t]) + bbeta_r)
         g_rs[t] = sigmoid(np.dot(Wog_r,os[t]) + bg_r)
@@ -232,34 +211,6 @@ class NTM(object):
                                              , w_ws[t-1]
                                              , mems[t-1])
 
-        if False:
-          print "computing the write weights"
-          print "k_ws[t]"
-          print k_ws[t]
-
-          print "beta_ws[t]"
-          print beta_ws[t]
-
-          print "g_ws[t]"
-          print g_ws[t]
-
-          print "s_ws[t]"
-          print s_ws[t]
-
-          print "gamma_ws[t]"
-          print gamma_ws[t]
-
-          print "w_ws[t-1]"
-          print w_ws[t-1]
-
-          print "mems[t-1]"
-          print mems[t-1]
-
-          print "w_ws[t]"
-          print w_ws[t]
-          #sys.exit(1)
-          pass
-
         w_rs[t] = addressing.create_weights(   k_rs[t]
                                              , beta_rs[t]
                                              , g_rs[t]
@@ -268,74 +219,18 @@ class NTM(object):
                                              , w_rs[t-1]
                                              , mems[t-1])
 
-        if False:
-          print "computing the read weights"
-          print "k_rs[t]"
-          print k_rs[t]
-
-          print "beta_rs[t]"
-          print beta_rs[t]
-
-          print "g_rs[t]"
-          print g_rs[t]
-
-          print "s_rs[t]"
-          print s_rs[t]
-
-          print "gamma_rs[t]"
-          print gamma_rs[t]
-
-          print "w_rs[t-1]"
-          print w_rs[t-1]
-
-          print "mems[t-1]"
-          print mems[t-1]
-
-          print "w_rs[t]"
-          print w_rs[t]
-
-        if False:
-            print "w_rs[t]"
-            print w_rs[t]
-            print "w_ws[t]"
-            print w_ws[t]
-
         ys[t] = np.dot(Woy, os[t]) + by
         ps[t] = sigmoid(ys[t])
 
         one = np.ones(ps[t].shape)
         ts[t] = np.reshape(np.array(targets[t]),(self.out_size,1))
-        if False:
-          print "ps[t]" 
-          print ps[t]
-          print "ts[t]"
-          print ts[t]
+
         epsilon = 2**-23 # to prevent log(0)
         a = np.multiply(ts[t] , np.log(ps[t] + epsilon))
         b = np.multiply(one - ts[t], np.log(one-ps[t] + epsilon))
         loss = loss - (a + b)
-        if False:
-          print "loss"
-          print loss
 
         # read from the memory
-        # TODO: are we reading from last memory or this one?
-          # sort of seems like no point reading from this one
-          # if you're going to write something just to read it out this 
-          # time, why not just write that straight to output10
-        if False:
-          tol = 0.02
-          if np.abs(np.sum(w_rs[t]) - 1) > tol:
-            print "read sum is wrong"
-            print w_rs[t]
-            print np.abs(np.sum(w_rs[t]) - 1)
-            sys.exit(1)
-          if np.abs(np.sum(w_ws[t]) - 1) > tol:
-            print "write sum is wrong"
-            print w_ws[t]
-            print np.abs(np.sum(w_ws[t]) - 1)
-            sys.exit(1)
-
         rs[t] = memory.read(mems[t-1],w_rs[t])
 
         # write into the memory
@@ -355,15 +250,5 @@ class NTM(object):
 
     loss, ps, reads, writes, adds, erases = fprop(self.weights)
     deltas = bprop(self.weights)
-    if False:
-      print "PARAMETERS AND DELTAS"
-      for n,d,p in zip(self.names, deltas, self.weights):
-        print "NAME:"
-        print n
-        print "PARAMS:"
-        print p 
-        print "DELTAS:"
-        print d
-        pass
 
     return loss, deltas, ps, reads, writes, adds, erases
