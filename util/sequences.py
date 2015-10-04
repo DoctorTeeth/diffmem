cdb1660e05b8a67bf8669b72af9d4401497d8ce0
Augustus Odena <odenaaugustus@gmail.com>
2015-10-04 15:56:56 -0700

    Add first draft of associative recall task.

--- util/sequences.py
+++ util/sequences.py
@@ -102,6 +102,51 @@ def repeat_copy(seq_len, vec_size, repeats):
   outputs[b] = finish_vec
   return inputs, outputs, seq_len
 
+def associative_recall(seq_len, vec_size, item_size):
+  """
+  We show between 2 and 6 items (seq len), each of which is 3 six bit binary vectors
+  For i in range(seq_len):
+    show a start bit, then item_size vectors
+  then show a fetch bit, then item_size vectors
+  then a final fetch bit
+  then we expect item_size vectors to be reproduced
+
+  so our total length is (seq_len+1)*(item_size+1) + 1 + seq_len
+  """
+  input_size  = vec_size + 2
+  output_size = vec_size
+  length  = (seq_len+1)*(item_size+1) + 1 + seq_len
+  inputs  = np.zeros((length,input_size),dtype=np.float32)
+  outputs = np.zeros((length,output_size),dtype=np.float32)
+
+  start_bit = np.zeros((1,input_size))
+  start_bit[0,-2] = 1
+  fetch_bit = np.zeros((1,input_size))
+  fetch_bit[0,-1] = 1
+
+  # generate seq_len random items
+  items = []
+  for i in range(seq_len):
+    items.append(np.random.randint(2, size=(item_size, vec_size)))
+    a = i*(item_size+1)
+    b = a + item_size
+    inputs[a] = start_bit
+    inputs[a+1:b+1,:-2] = items[i] 
+
+  # pick an item at random that isn't the last item
+  idx = np.random.randint(low=0, high=len(items) - 1) 
+  fetch_item = items[idx]
+
+  inputs[b+1] = fetch_bit
+  inputs[b+2:b+2+item_size, :-2] = fetch_item
+  inputs[b+2 + item_size] = fetch_bit
+  # choose a random item to be the prompt
+
+  # pdb.set_trace()
+  outputs[b+2+item_size+1:inputs.shape[0]] = items[idx+1]
+
+  return inputs, outputs, seq_len
+
 class SequenceGen(object):
 
   def __init__(self, sequenceType, vec_size, hi, lo):
@@ -122,3 +167,7 @@ class SequenceGen(object):
       self.make = make
     else:
       raise NotImplementedError
+
+i, t, l = associative_recall(3, 3, 3)
+print i
+print t
