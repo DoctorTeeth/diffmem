05fb162ae1c8c1f0f4c3addc0c6aa76b73e3856d
Augustus Odena <odenaaugustus@gmail.com>
2015-10-05 17:09:26 -0700

    First draft with all 5 tasks usable.

--- util/sequences.py
+++ util/sequences.py
@@ -3,15 +3,40 @@ Generate random instances of various artificial tasks.
 Right now, only a bit-vector copy task is implemented.
 """
 import autograd.numpy as np
+import itertools
 import pdb
+
+def ngram_table(bits):
+  """
+  Generate a table that contains the probability of seeing a 1
+  after having seen a context of length bits-1
+  """
+  assert(bits >= 2)
+  prod = itertools.product([0,1], repeat=bits-1)
+  table = {}
+  for p in prod:
+    table[p] = np.random.beta(0.5,0.5)
+
+  return table
+
+def sample_ngram(table, context, n):
+  """
+  given an n-gram table and the current context, generate the next bit
+  """
+  p = table[tuple(context.reshape(1,n-1).tolist()[0])]
+  r = np.random.uniform(low=0, high=1)
+  if r > p:
+    return 1
+  else:
+    return 0
 
 def copy_sequence(seq_len, vec_size):
   """
   Returns inputs, outputs
   where inputs is a length 2 * seq_len + 2 sequence of vec_size + 2 vecs
   and outputs is a length 2 * seq_len + 2 sequence of vec_size vecs
-  inputs starts with a start bit, then the seq to be copied, then and end bit, then 0s 
-  outputs is 0s until after inputs has the end bit, then it's the first sequence, but without 
+  inputs starts with a start bit, then the seq to be copied, then and end bit, then 0s
+  outputs is 0s until after inputs has the end bit, then it's the first sequence, but without
   the extra bits
   """
   input_size = vec_size + 2
@@ -186,40 +211,39 @@ def priority_sort(seq_len, vec_size):
 
   return inputs, outputs, seq_len
 
-def ngrams(seq_len, vec_size):
+def ngrams(seq_len, n):
   """
   this is the dynamic n-grams task
+  for n, the set of all possible n-gram distributions over binary sequences is
+  specified with 2^(n-1) probabilities, the probability of seeing a 1
+  given that the last n-1 bits were what they were
+
+  we then draw 200 bits from the current lookup table to make a training sequence
+  the first 5 bits are drawn iid from a bernoulli distribution with p=0.5
+
+  the optimal estimator works as follows
+  where c is the previous 5-bit context, if n0 and n1 are the number of
+  0s and 1s previously observed in that context
+
+  p(B=1 | n0,n1,c) = (n1 + 0.5) / (n0 + n1 + 1)
+
+  for each training example, generate a 6gram distribution
+  then draw 200 bits from that distribution and use them as training
+
+  we won't worry about validation at this layer, because
+  we can validate using test mode
   """
-  input_size  = vec_size + 1
-  output_size = vec_size
-  length  = 2*seq_len + 2
-  inputs  = np.zeros((length,input_size),dtype=np.float32)
-  outputs = np.zeros((length,output_size),dtype=np.float32)
+  beta_table = ngram_table(n)
+  inputs  = np.zeros((seq_len,1))
+  outputs = np.zeros((seq_len,1))
 
-  start_bit = np.zeros((1,input_size))
-  start_bit[0,-2] = 1
-  stop_bit = np.zeros((1,input_size))
-  stop_bit[0,-1] = 1
-
-  inputs[0] = start_bit
-  inputs[seq_len + 1] = stop_bit
-  # now randomly generate seq_len vectors with their
-  # priorities
-  items = []
-  for i in range(0,seq_len):
-    seq = np.random.randint(2, size=(1, vec_size))
-    priority = np.random.uniform(low=-1, high=1)
-    items.append((seq, priority))
-    inputs[i+1,:-1] = seq
-    inputs[i+1,-1] = priority
-
-  items.sort(key=lambda x: x[1])
-
-  # fill in the outputs
-  for i in range(0,seq_len):
-    seq, _ = items[i]
-    outputs[seq_len + 2 + i] = seq
-
+  for i in range(seq_len):
+    if i < n - 1:
+      inputs[i] = np.random.randint(2)
+    else:
+      inputs[i] = sample_ngram(beta_table, inputs[i - n + 1:i],n)
+  # the task is simply to predict the next bit
+  outputs[:-1] = inputs[1:]
   return inputs, outputs, seq_len
 
 class SequenceGen(object):
@@ -249,9 +273,20 @@ class SequenceGen(object):
         seq_len = np.random.randint(lo, hi + 1)
         return associative_recall(seq_len, vec_size, item_size)
       self.make = make
+    elif sequenceType == 'priority_sort':
+      self.out_size = vec_size
+      self.in_size  = vec_size + 2
+      def make():
+        seq_len = np.random.randint(lo, hi + 1)
+        return priority_sort(seq_len, vec_size)
+      self.make = make
+    elif sequenceType == 'ngrams':
+      self.out_size = 1
+      self.in_size  = 1
+      n = 6 # this is hardcoded for now
+      def make():
+        seq_len = np.random.randint(lo, hi + 1)
+        return ngram(seq_len, n)
+      self.make = make
     else:
       raise NotImplementedError
-
-# i, t, l = ngrams(2, 3)
-# print i
-# print t
