49d39abbfe456e5a7e8138a03c4ed7b7da9d0fe0
Augustus Odena <odenaaugustus@gmail.com>
2015-10-08 14:56:55 -0700

    Fix lint errors

--- ntm/memory.py
+++ ntm/memory.py
@@ -5,7 +5,7 @@ via weight vectors as in section 3.
 
 import autograd.numpy as np
 
-def read(mem,weight_vector):
+def read(mem, weight_vector):
     """
     The reading procedure as described in 3.1.
     weight_vector (w_t below) is a weighting over the rows such that:
@@ -14,9 +14,9 @@ def read(mem,weight_vector):
     We return \sum_i w_t(i) M_t(i); M_t(i) is the i-th row of memory.
     """
 
-    return np.dot(weight_vector.T,mem)
+    return np.dot(weight_vector.T, mem)
 
-def write(mem,w_t,e_t,a_t):
+def write(mem, w_t, e_t, a_t):
     """
     The writing procedure as described in 3.2.
     w_t is a length N weighting over the rows as above.
@@ -28,12 +28,12 @@ def write(mem,w_t,e_t,a_t):
         inspired by the forget/input gates in LSTM.
     """
     # Perform erasure on the existing memory, parametrized by e_t and w_t
-    W = np.reshape(w_t,(w_t.shape[0],1))
-    E = np.reshape(e_t,(e_t.shape[0],1))
+    W = np.reshape(w_t, (w_t.shape[0], 1))
+    E = np.reshape(e_t, (e_t.shape[0], 1))
 
     # Transpose W so we can create WTE, a matrix whose i,j-th element
         # represents the extent to which we will erase M_t[i,j]
-    WTE = np.dot(W,E.T)
+    WTE = np.dot(W, E.T)
 
     # KEEP is such that KEEP[i,j] represents the extent to which we
         # will keep M_t[i,j]
@@ -44,11 +44,11 @@ def write(mem,w_t,e_t,a_t):
 
     # Perform addition on the newly erased memory
     # Convert add vector to a matrix
-    A = np.reshape(a_t,(a_t.shape[0],1))
+    A = np.reshape(a_t, (a_t.shape[0], 1))
 
     # Add is the add vector weighted by w_t, which is added pointwise to
         # the existing memory, finishing the write sequence.
-    ADD = np.dot(W,A.T)
+    ADD = np.dot(W, A.T)
     newmem = newmem + ADD
 
     return newmem
